7e22374c378b12faae026d92307a88af5002936c
ThesllaDev <theslladev@gmail.com>
2022-08-12 13:02:52 -0300

    🗃️ Chore: update data with project deployment url

--- src/App.jsx
+++ src/App.jsx
@@ -6,84 +6,104 @@ import { Home } from "./assets/components/pages/Home";
 
 const PROJECTDATAS = [
 	{
-		id: 1,
+		id: "01",
 		title: "Ícones de mídias sociais em camada",
+		deploy: "https://codepen.io/theslladev/full/NWYgoNy",
 	},
 	{
-		id: 2,
+		id: "02",
 		title: "Loaders animados",
+		deploy: "https://codepen.io/theslladev/full/zYWdvaw",
 	},
 	{
-		id: 3,
+		id: "03",
 		title: "Letreiro com mudança de cor de texto",
+		deploy: "https://codepen.io/theslladev/full/ZExJXoZ",
 	},
 	{
-		id: 4,
+		id: "04",
 		title: "Botões com efeito de borda, hover e click",
+		deploy: "https://codepen.io/theslladev/full/MWVEaQw",
 	},
 	{
-		id: 5,
+		id: "05",
 		title: "Joias do infinito com efeito de pulsar",
+		deploy: "https://codepen.io/theslladev/full/WNzZLob",
 	},
 	{
-		id: 6,
+		id: "06",
 		title: "Efeito de animação do texto brilhando",
+		deploy: "https://codepen.io/theslladev/full/WNzXRmW",
 	},
 	{
-		id: 7,
+		id: "07",
 		title: "Preloader com efeito de ondulação",
+		deploy: "https://codepen.io/theslladev/full/VwXrRjj",
 	},
 	{
-		id: 8,
+		id: "08",
 		title: "Radar animado",
+		deploy: "https://codepen.io/theslladev/full/VwXyevr",
 	},
 	{
-		id: 9,
+		id: "09",
 		title: "O pêndulo de Newton",
+		deploy: "https://codepen.io/theslladev/full/yLKpvKz",
 	},
 	{
-		id: 10,
+		id: "10",
 		title: "Animação de troca de emojis",
+		deploy: "https://codepen.io/theslladev/full/ZExvdLq",
 	},
 	{
-		id: 11,
+		id: "11",
 		title: "Botões de ícone com efeito hover",
+		deploy: "https://codepen.io/theslladev/full/LYdQQpd",
 	},
 	{
-		id: 12,
+		id: "12",
 		title: "Animação de preenchimento ao passar o mouse",
+		deploy: "https://codepen.io/theslladev/full/vYRRLym",
 	},
 	{
-		id: 13,
+		id: "13",
 		title: "Placeholders loadings",
+		deploy: "https://codepen.io/theslladev/full/ExEEOdr",
 	},
 	{
-		id: 14,
+		id: "14",
 		title: "Fundo de texto animado com efeito parallax",
+		deploy: "https://codepen.io/theslladev/full/bGvMgdV",
 	},
 	{
-		id: 15,
+		id: "15",
 		title: "Animação de texto esfumaçado",
+		deploy: "https://codepen.io/theslladev/full/OJvZBBq",
 	},
 	{
-		id: 16,
+		id: "16",
 		title: "Botão com efeito de pressionamento de tecla",
+		deploy: "https://codepen.io/theslladev/full/abYKzKo",
 	},
 	{
-		id: 17,
+		id: "17",
 		title: "Animação de bateria carregando",
+		deploy: "https://codepen.io/theslladev/full/xxWzWPj",
 	},
 	{
-		id: 18,
+		id: "18",
 		title: "Texto flutuante e imersivo",
+		deploy: "https://codepen.io/theslladev/full/XWEBppM",
 	},
 	{
-		id: 19,
+		id: "19",
 		title: "Animação partículas de fundo",
+		deploy: "https://codepen.io/theslladev/full/Yzajgye",
 	},
 	{
-		id: 20,
+		id: "20",
 		title: "Botão com efeito hover de arco-íris",
+		deploy: "https://codepen.io/theslladev/full/WNzgOvm",
 	},
 ];
 
