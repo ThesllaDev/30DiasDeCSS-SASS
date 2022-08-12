85a2206a47248070750674485c0f7bfe39b90b37
ThesllaDev <theslladev@gmail.com>
2022-08-12 15:03:18 -0300

    🚸🔍Feat: Improvements with Abbr Tags in Abbreviations

--- src/assets/components/pages/Home.jsx
+++ src/assets/components/pages/Home.jsx
@@ -9,14 +9,19 @@ export const Home = ({ projectsDatas }) => {
 		<main>
 			<header id="about" className="about-section">
 				<h1>
-					#30 Dias de CSS <FaCss3Alt />
+					#30 Dias de <abbr title="Cascading Style Sheets">CSS</abbr>
+					<FaCss3Alt />
 				</h1>
 				<p className="about-section__description">
-					30 dias de CSS, é um desafio para ajudá-lo a melhorar suas habilidades
-					de codificação fazendo mini projetos de animações diariamente durante
-					30 dias, utilizando HTML e CSS (no momento que estou realizando esse
-					projeto também estou aprendendo sobre o SASS que é um pré-processador
-					CSS, então também decidi adiciona-lo ao desafio)
+					30 dias de CSS(Cascading Style Sheets), é um desafio para ajudá-lo a
+					melhorar suas habilidades de codificação fazendo mini projetos de
+					animações diariamente durante 30 dias, utilizando <abbr>HTML</abbr>
+					(HyperText Markup Language) e{" "}
+					<abbr title="Cascading Style Sheets">CSS</abbr> (no momento que estou
+					realizando esse projeto também estou aprendendo sobre o{" "}
+					<abbr>SASS</abbr>(Syntactically Awesome Style Sheets) que é um
+					pré-processador <abbr title="Cascading Style Sheets">CSS</abbr>, então
+					também decidi adicioná-lo ao desafio)
 				</p>
 				<p>
 					Para mais informações consulte o{" "}
