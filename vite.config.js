e4efe4899a893bbcea4d96dc2550a07fa735e7c7
ThesllaDev <theslladev@gmail.com>
2022-08-13 15:28:46 -0300

    🚀Chore: deploy project on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	base: "https://ThesllaDev.github.io/30DiasDeCSS-SASS/",
+	build: {
+		outDir: "build",
+	},
+	plugins: [react()],
+});
